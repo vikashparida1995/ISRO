dc816479c1011a6e148e2433fb4cc8b47367a02b
vikashparida1995 <vikash.parida1995@gmail.com>
2025-07-08 14:41:33 +0530

    impleament logs in app using morgan package

--- server/src/app.js
+++ server/src/app.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const morgan = require('morgan');
 
 const cors = require('cors')
 
 app.use(cors({
     origin:'http://localhost:3000'
 }))
+
+app.use(morgan('dev'));
+app.use(morgan('common'));
+app.use(morgan('tiny'));
+app.use(morgan('combined'))
 app.use(express.json());
 app.use(express.static(path.join(__dirname ,'..', 'public')))
 
