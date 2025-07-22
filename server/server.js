6bbfa42b460914949f80ea1001223e79462b2a85
vikashparida1995 <vikash.parida1995@gmail.com>
2025-07-22 18:06:21 +0530

    connect mongodb atles using mongoose and atles url string

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
+require('dotenv').config();
 const http = require('http')
 const port = process.env.PORT || 8000;
+mongoose = require('mongoose');
 
 const app = require('./src/app')
 
@@ -13,6 +15,20 @@ const server = http.createServer(app)
 
 
 async function  loadData() {
+
+  mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,  
+    useUnifiedTopology: true,
+  })
+  .then(() => {   
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {   
+    console.error('Error connecting to MongoDB:', error);
+
+    process.exit(1); // Exit the process if connection fails
+
+  }); 
     loadPlanetsData()
   .then(() => {
     console.log('Planets data loaded successfully');
